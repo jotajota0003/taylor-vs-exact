77a352628d9ced9ca36dea7bb8b966dc6cfa5edd
jotajota0003 <90326871+jotajota0003@users.noreply.github.com>
2026-02-22 16:46:00 -0300

    Merge branch 'master' of https://github.com/jotajota0003/XamarimForms

---
+++ 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServicoBase.cs
@@ -0,0 +1,23 @@
+﻿using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServicoBase
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
